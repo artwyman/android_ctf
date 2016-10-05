6dd3c72e1052766b61a4b7c8f40f0e91461488f7
Andrew Twyman <atwyman@dropbox.com>
2016-10-04 20:28:17 -0700

    Flag in string resource

---
+++ README.md
@@ -0,0 +1,4 @@
+Android problem for Dropbox Security CTF.
+Flag is embedded in the app, with varying levels of obfuscation available on different branches.
+Flag value is hard-coded as "insert_flag_here" in sample code. 
+Signed APK is available in signed_apks directory.
---
+++ app/src/main/java/com/dropbox_ctf/flagapp/FlagActivity.java
@@ -0,0 +1,31 @@
+package com.dropbox_ctf.flagapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class FlagActivity extends AppCompatActivity {
+    private static String TAG = "com.dropbox_ctf.flagapp.FlagActivity";
+    private TextView mFlagEntryView;
+    private TextView mFlagResultView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag);
+
+        mFlagEntryView = (TextView)findViewById(R.id.flag_entry);
+        mFlagResultView = (TextView)findViewById(R.id.flag_result);
+    }
+
+    public void onClickTest(View v) {
+        Log.i(TAG, "Test: " + mFlagEntryView.getText());
+        if (mFlagEntryView.getText().toString().equals(getString(R.string.flag_value))) {
+            mFlagResultView.setText(R.string.flag_result_yes);
+        } else {
+            mFlagResultView.setText(R.string.flag_result_no);
+        }
+    }
+}
